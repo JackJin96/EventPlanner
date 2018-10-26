3792fa812debcc71c8f1952135baf0a106a489ae
Ellen Mak <ellenmak@dhcp-wifi-8021x-155-41-118-150.bu.edu>
2018-10-25 20:28:30 -0400

    html and ts updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EventPlanner';
-}
+  
+  location = "";
+  date = "";
+
+  submitted = false;
+
+  onSubmit(form: NgForm){
+    this.submitted = true;
+    this.location = form.form.value.location;
+    this.date = form.form.value.date;
+  }
+
+  }
+
+  
+
+
