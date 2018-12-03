e17178255dcfd748aab622c57c57f1a02454a4b3
Jack <hjin96@bu.edu>
2018-12-02 19:07:52 -0500

    moved jquery code in navbar to html header

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -26,16 +26,4 @@ export class NavbarComponent implements OnInit {
     });
   }
 
-  $(function(){
-    var $page = jQuery.url.attr("file");
-    $('ul.navigation li a').each(function(){
-        var $href = $(this).attr('href');
-        if ( ($href == $page) || ($href == '') ) {
-            $(this).addClass('on');
-        } else {
-            $(this).removeClass('on');
-        }
-    });
-});
-
 }
